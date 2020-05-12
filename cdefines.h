d3f54e008171aae591eac6b85882ad5a9b738335
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-05-12 23:36:57 +0200

    Contains column numbers (begin/end) for each line of an TLE as C definitions

---
+++ cdefines.h
@@ -0,0 +1,82 @@
+#ifndef CDEFINES_H
+#define CDEFINES_H
+
+// --LINE 1
+
+#define Line_Number 1
+#define Line_Number_End 1
+
+#define Satellite_Number 3
+#define Satellite_Number_End 7
+
+#define Classification 8
+#define Classification_End 8
+
+#define International_Designator_Year 10
+#define International_Designator_Year_End 11
+
+#define International_Designator_NoOfYear 12
+#define International_Designator_NoOfYear_End 14
+
+#define International_Designator_PieceOfLaunch 15
+#define International_Designator_PieceOfLaunch_End 17
+
+#define Epoch_Year 19
+#define Epoch_Year_End 20
+
+#define Epoch_Day_Fraction 21
+#define Epoch_Day_Fraction_End 32
+
+#define First_Time_Derivative_Mean_Motion 34
+#define First_Time_Derivative_Mean_Motion_End 43
+
+#define Second_Time_Derivative_Mean_Motion 45
+#define Second_Time_Derivative_Mean_Motion_End 52
+
+#define BSTAR 54
+#define BSTAR_End 61
+
+#define Ephemeris 63
+#define Ephereris_End 63
+
+#define Element_Number 65
+#define Element_Number_End 68
+
+#define Checksum 69
+#define Checksum_End 69
+
+
+// --LINE 2
+// ## Line Number identisch zu Line 1!
+
+// ## Satellite Number identisch zu Line 1!
+
+#define Inclination 9
+#define Inclination_End 16
+
+#define Right_Ascension_Of_Ascending_Node 18
+#define Right_Ascension_Of_Ascending_Node_End 25
+
+#define Eccentricity 27
+#define Eccentricity_End 33
+
+#define Argument_Of_Perigee 35
+#define Argument_Of_Perigee_End 42
+
+#define Mean_Anomaly 44
+#define Mean_Anomaly_End 51
+
+#define Mean_Motion 53
+#define Mean_Motion_End 63
+
+#define Revolution_Number_At_Epoch 64
+#define Revolution_Number_At_Epoch_End 68
+
+// ## Checksum identisch zu Line 1!
+
+
+// Aufgabe 1.2 (Sonstiges)
+#define SONATE_NUMBER 44400
+
+
+#endif // CDEFINES_H
