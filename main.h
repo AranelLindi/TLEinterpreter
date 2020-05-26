ac819494543e01e97dbc40392ce2e2299e955cc4
Stefan Lindörfer <65138334+AranelLindi@users.noreply.github.com>
2020-05-26 22:37:55 +0200

    Update main.h

--- main.h
+++ main.h
@@ -9,9 +9,14 @@
 
 // Diesen Header eventuell umbennen und includen um TLE Interpreter in neues Projekt einzubinden.
 
+#ifndef MAIN_H
+#define MAIN_H
+
 // Relevant Header-Files
 #include "TLE.h"
 #include "process.h"
 
 // Global Variables
 extern bool SONATE_only; // Was only needed for an exercise
+
+#endif // main.h
